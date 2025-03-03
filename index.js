4fec70ea90cf85326163aacebf6a576ab452231e
wesleynjerenga <wesleynjerenga9@gmail.com>
2025-03-03 15:17:47 +0300

    modifying-elements

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main=document.getElementById('main');
+if(main){
+    main.remove();
+}
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Wesley is the champion";
+document.body.appendChild(newHeader); 
